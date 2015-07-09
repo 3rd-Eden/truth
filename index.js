2f5605c0c4e4da270a5b2394b2858b4665d72b97
Swaagie <info@martijnswaagman.nl>
2015-07-09 14:27:30 +0200

    [fix] clone with options

--- index.js
+++ index.js
@@ -298,10 +298,11 @@ Truth.prototype.get = function get() {
  * Create a clone of the store so we can easily inherit the transformations.
  *
  * @param {String} name Name of the truth store.
+ * @param {Object} options Optional configuration.
  * @api public
  */
-Truth.prototype.clone = function clone(name) {
-  var truth = new Truth(name);
+Truth.prototype.clone = function clone(name, options) {
+  var truth = new Truth(name, options);
 
   truth.transforms = this.transforms.slice();
   return truth;
